bb3ebc1346841ff9ed9bb5371abde91c1ed6a7f9
PhucHauNguyenVo <hauphucnguyenvo@gmail.com>
2025-10-12 01:38:01 +0700

    Cập nhật backend: kết nối MongoDB thành công

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/user');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected successfully'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
 
 // ⚡ Bắt buộc phải có dòng này để Express đọc được JSON từ Postman
 app.use(express.json());
