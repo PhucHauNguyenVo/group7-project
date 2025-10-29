d6ba4c14376067986a79cd7be60e87edac303133
phamtrungtinhh <phamtrungtinnh@gmail.com>
2025-10-29 17:58:54 +0700

    Hoàn tất hợp nhất code, xử lý conflict file user.js

---
+++ backend/models/User_backup.js
@@ -0,0 +1,8 @@
+const User = require('./userModel');
+
+module.exports = User;
+module.exports.USER_ROLES = User.USER_ROLES;
+const User = require('./userModel');
+
+module.exports = User;
+module.exports.USER_ROLES = User.USER_ROLES;
